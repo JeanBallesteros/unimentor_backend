d896b03d31ca888a01bf93e526583d935a562c51
JeanBallesteros <jeanc.ballesterosz@autonoma.edu.co>
2024-03-15 21:20:24 -0500

    CORS y VERCEL

--- app.js
+++ app.js
@@ -26,11 +26,27 @@ app.use(`/${process.env.API_PATH}/users`, userRoutes);
 app.use(`/${process.env.API_PATH}/auth`, authRoutes);
 
 // Configuración de cabeceras CORS para permitir métodos HTTP
+// app.use((req, res, next) => {
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     next();
+// });
+
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+      res.header(
+        "Access-Control-Allow-Methods",
+        "POST, PUT, PATCH, GET, DELETE"
+      )
+      return res.status(200).json({})
+    }
+    next()
+})
 
 module.exports = app
